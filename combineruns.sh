cb8130a833c1bba384aafa2f61fcbb0012e40b0a
rmrodell <koalabear@KoalaBear.localdomain>
2024-11-28 09:38:05 +1300

    adding comments and defined directory

--- combineruns.sh
+++ combineruns.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Define the source directories
-dir1="BR_pool1/20241112/fastq"
-dir2="BR_pool1/20241113/fastq"
-target_dir="BR_pool1/fastq"
+dir1="/scratch/users/rodell/20241114_pool1/BR_pool1/20241112/fastq"
+dir2="/scratch/users/rodell/20241114_pool1/BR_pool1/20241113/fastq"
+target_dir="/scratch/users/rodell/20241114_pool1/BR_pool1/fastq"
 
 # Loop through each FASTQ file in the first directory
 for file in "$dir1"/*.fq; do
     # Extract the base filename (without path)
     filename=$(basename "$file")
-    
+    echo "Processing $filename"
     # Check if the corresponding file exists in the second directory
     if [[ -f "$dir2/$filename" ]]; then
         # Concatenate files and write to the target directory
