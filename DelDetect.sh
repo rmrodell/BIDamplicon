a159108b6060a150bc8cc2f4f9ef5a8d58ac27f5
rmrodell <rodell@stanford.edu>
2025-01-08 13:21:20 -0800

    concatenating counts from bam file

---
+++ DelDetect.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# first argument is directory with find sorted bam files NO FINAL SLASH
+# second argument is where outputs should go
+# third argument is path to reference fasta file
+# fourth argument is path to bed file with regions to examine
+
+
+
+
+# Base path to the sorted & indexed bam files
+BAM_PATH="$1"
+DEST_PATH="$2"
+
+# Name other input files
+ref_fa="$3"
+bed="$4"
+
+# name where final counts will go
+count_file="${DEST_PATH}/deldetect_counts.txt"
+
+# Initialize the final output file with headers
+echo "sample    chr pos gene    totalReads  A.count C.count G.count T.count Deletion.count  Insertion.count reference   kmer    strand" > "$count_file"
+
+(cd $BAM_PATH
+# Loop through each .bam file in the input directory
+for bam_file in "$BAM_PATH"/*.bam; do
+
+    sample=$(basename "${bam_file}" _sort.bam)  # Get the base name without the extension
+
+    echo "Processing sample: ${sample}"
+
+    # extract counts using bam_counts.R
+    Rscript bam_count.R --bedFile $bed --bamFile $bam_file --referenceFasta $ref_fa --outputFile ${sample}_counts.txt
+
+    # Process the resultant file and append it to the final output
+    result_file="${sample}_counts.txt"
+
+    # Append the rest of the lines (skip header)
+    awk -v sample="$sample" 'NR > 1 {print sample "," $0}' "$result_file" >> "$count_file"
+
+)
+
+echo "All files counted. Final count file located at $count_file" 
+
+# split sample name into different columns
