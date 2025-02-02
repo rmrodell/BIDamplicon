d5ce5e317b08799059186e10d6fbeb7b348087fe
rmrodell <koalabear@KoalaBear>
2025-02-01 19:38:16 -0800

    added strandedness

--- regions.sh
+++ regions.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-# this version does NOT pay attention to strandedness
 BED=$1 # first argument is bed file with sites of interest
 
 ml biology bedtools
@@ -8,17 +7,17 @@ ml biology bedtools
 echo "Examining $BED"
 
 # intersect with 5'UTR
-num_5utr=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_5utr.bed -u | wc -l)
+num_5utr=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_5utr.bed -u -s | wc -l)
 echo "Number of sites in 5' UTR: $num_5utr"
 
 # interest with 3' UTR
-num_3utr=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_3utr.bed -u | wc -l)
+num_3utr=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_3utr.bed -u -s | wc -l)
 echo "Number of sites in 3' UTR: $num_3utr"
 
 # interest with exons
-num_exons=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_codingexons.bed -u | wc -l)
+num_exons=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_codingexons.bed -u -s | wc -l)
 echo "Number of sites in exons: $num_exons"
 
 # interest with 3' UTR
-num_introns=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_introns.bed -u | wc -l)
+num_introns=$(bedtools intersect -a "$BED" -b /home/users/rodell/regions/hg38_introns.bed -u -s | wc -l)
 echo "Number of sites in introns: $num_introns"
