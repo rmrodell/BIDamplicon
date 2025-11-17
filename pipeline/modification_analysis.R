678c93a5a44b9239e3a583c9912f052a6953cbbb
rmrodell <rodell@stanford.edu>
2025-11-17 08:32:07 -0800

    increased figure resolution

--- pipeline/modification_analysis.R
+++ pipeline/modification_analysis.R
@@ -422,7 +422,7 @@ if (nrow(modified_sites) >= 4) {
 
     plot_path <- file.path(plot_dir, paste0(args$prefix, "_boxplot_modified_quartiles"))
     ggsave(paste0(plot_path, ".pdf"), p_quartile_boxplot, width = 4, height = 4, units = "in")
-    ggsave(paste0(plot_path, ".png"), p_quartile_boxplot, width = 4, height = 4, units = "in")
+    ggsave(paste0(plot_path, ".png"), p_quartile_boxplot, width = 4, height = 4, units = "in", dpi = 600)
 
     # Save data for each quartile
     modified_sites_quartiles %>%
@@ -506,7 +506,7 @@ if (nrow(modified_sites) > 0) {
 
     plot_path <- file.path(plot_dir, paste0(args$prefix, "_boxplot_modified_summary"))
     ggsave(paste0(plot_path, ".pdf"), p_box, width = 4, height = 4, units = "in")
-    ggsave(paste0(plot_path, ".png"), p_box, width = 4, height = 4, units = "in")
+    ggsave(paste0(plot_path, ".png"), p_box, width = 4, height = 4, units = "in", dpi = 600)
     cat("Summary boxplot of modified sites saved.\n")
 }
 
@@ -542,7 +542,7 @@ if (nrow(modified_sites) > 0) {
 
     plot_path_nolabels <- file.path(plot_dir, paste0(args$prefix, "_heatmap_modified_summary_nolabels"))
     ggsave(paste0(plot_path_nolabels, ".pdf"), p_heatmap_nolabels, width = 4, height = 6, units = "in")
-    ggsave(paste0(plot_path_nolabels, ".png"), p_heatmap_nolabels, width = 4, height = 6, units = "in")
+    ggsave(paste0(plot_path_nolabels, ".png"), p_heatmap_nolabels, width = 4, height = 6, units = "in", dpi = 600)
     cat("Summary heatmap without labels saved.\n")
 
     p_heatmap_labels <- p_heatmap_base +
